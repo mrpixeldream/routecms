b076c4d6fed4efadacb778b8f75aa37633720596
Olaf Braun <olaf_schmitz_1@t-online.de>
2015-02-15 17:18:44 +0100

    Bearbeitung der Pfad definition

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,12 @@
 <?php
 if(!defined('DIRNAME'))
-	define('DIRNAME', str_replace('\\', '/', dirname(__FILE__)).'/../');
+{
+    define('DIRNAME', str_replace('\\', '/', dirname(__FILE__)).'/../');
+}
 if(!defined('ADMIN'))
-	define('ADMIN', true);
+{
+    define('ADMIN', true);
+} 
 require_once(DIRNAME.'lib/Admin.php');
 $admin = new Admin();
 $admin->startTemplate();
--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 if(!defined('DIRNAME'))
-	define('DIRNAME', str_replace('\\', '/', dirname(__FILE__)).'/');
+{
+    define('DIRNAME', str_replace('\\', '/', dirname(__FILE__)).'/../');
+} 
 require_once(DIRNAME.'lib/Routecms.php');
 $routecms = new Routecms();
 $routecms->startTemplate();
